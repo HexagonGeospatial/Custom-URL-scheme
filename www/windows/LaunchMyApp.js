d43995f93e7dd578d1ceac7a4f3dec323f253bd3
Dawid Karpik <dkarpik@ingrnet.com>
2016-04-14 16:22:48 +0200

    Fixed firing handleOpenURL on cold start on Windows 10

--- www/windows/LaunchMyApp.js
+++ www/windows/LaunchMyApp.js
@@ -1,10 +1,11 @@
-(function () {
-	function activatedHandler(e) {
-		if (typeof handleOpenURL == 'function' && e.uri) {
-			handleOpenURL(e.uri.rawUri);
-		}
-	};
-	
-	var wui = Windows.UI.WebUI.WebUIApplication;
-	wui.addEventListener("activated", activatedHandler, false);
-}());
+ (function() {
+     function activatedHandler(e) {
+         if (typeof window.handleOpenURL == 'function' && e.detail.uri) {
+             window.handleOpenURL(e.detail.uri.rawUri);
+         } else if (e.detail.uri) {
+             window.customUrl = e.detail.uri.rawUri;
+         }
+     };
+
+     WinJS.Application.addEventListener("activated", activatedHandler, false);
+ }());
